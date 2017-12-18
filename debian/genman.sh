c6d61422e7ffbd8ecb53520b4e66c26bece0e4ea
johnraff <github@rafflesnagoya.com>
2017-12-18 15:46:59 +0900

    debian/genman.sh: add checks, support for other manual sections and existing manpages files

--- debian/genman.sh
+++ debian/genman.sh
@@ -1,15 +1,21 @@
 # genman.sh
 # script to generate man files from executables' --help options,
 # using help2man.
+# Tested on Dash, should work with other POSIX shells.
 
 # Put this script in the package source debian/ directory,
-# add a file genman-list or <packagename>.genman-list
+# add a file genman-list, <packagename>.genman-list
+# or <packagename>.<section-number>.genman-list
 # with a list of the executables whose manpages are to be built.
-# Shell globs may be used, but spaces in filenames or paths may not.
+# Shell globs may be used.
+# If set, <section-number> determines the manual section,
+# otherwise section 1 is used by default .
+# Multiple genman-list files can be used, for source building
+# multiple packages, or for different manual sections.
 #
 # Built manpages will be put in debian/genman-pages, and
 # their paths will be appended to an existing debian/*manpages file,
-# or put in a new one (following the naming rule of genman-list).
+# or put in a new manpages or <packagename>.manpages file.
 #
 # genman.sh --clean will return everything to its previous state.
 #
@@ -27,11 +33,18 @@
 
 set -e
 
-src_name=$(dpkg-parsechangelog -S Source)
+src_name=$(dpkg-parsechangelog -S Source) || {
+    echo "$0: Not in debian source directory?" >&2
+    exit 1
+}
 pkg_ver=$(dpkg-parsechangelog -S Version)
 
 workdir=debian # Should match the path set in debian/rules.
 
+# default if genman-list has no digit in name
+default_section=1
+
+# BunsenLabs generic content
 includes="[authors]
 Written by the BunsenLabs team.
 
@@ -76,13 +89,18 @@ build_mans() {
 mk_man() {
     local exec="$1"
     local cmd=${1##*/}
+    local manfile="${manpg_dir}/${cmd}.${section}"
+    grep "${cmd}\(.[1-8]\)\? *\$" "${manpages_file}" >/dev/null 2>&1 && {
+        echo "$0: ${cmd} already in ${manpages_file}, skipping"
+        return 0
+    }
     chmod +x "$exec"
     default_desc="a script provided by ${pkg_name}"
     desc="$( "$exec" --help | sed -rn "/^ *$cmd/ {s/^ *$cmd( -|:| is)? *//p;q}")"
     [ -z "$desc" ] && desc="$default_desc"
-    help2man "$exec" --no-info --no-discard-stderr --version-string="$cmd $pkg_ver" --name="$desc" --include="$include_file" | sed "s|$HOME|~|g" > "${manpg_dir}/${cmd}.1"
+    help2man "$exec" --no-info --no-discard-stderr --version-string="$cmd $pkg_ver" --section="$section" --name="$desc" --include="$include_file" | sed "s|$HOME|~|g" > "$manfile"
     chmod -x "$exec"
-    echo "${manpg_dir}/${cmd}.1" >> "${manpages_file}"
+    echo "$manfile" >> "${manpages_file}"
 }
 
 ### script starts here ###
@@ -93,10 +111,10 @@ case $1 in
     # avoid cleaning existing files
     [ -f "$include_file" ] && rm -f debian/*manpages
     rm -rf "$include_file"
-    for i in debian/*.genman.bak # restore backups
+    for i in debian/*.genman.bckp # restore backups
     do
         [ -f "$i" ] || continue
-        mv "$i" "${i%.genman.bak}"
+        mv "$i" "${i%.genman.bckp}"
     done
     exit 0
     ;;
@@ -112,7 +130,7 @@ esac
 for file in debian/*manpages
 do
     [ -f "$file" ] || continue
-    cp "$file" "$file".genman.bak
+    cp "$file" "$file".genman.bckp
 done
 
 cat <<EOF > "$include_file"
@@ -125,10 +143,22 @@ do
     found_list=true
     pkg_name="${list##*/}"
     pkg_name="${pkg_name%.genman-list}"
+# If filename is <package>.[1-9].genman-list
+# then digit determines section, otherwise use default set above.
+    case $pkg_name in
+    *.[1-8])
+        section="${pkg_name##*.}"
+        pkg_name="${pkg_name%.*}"
+        ;;
+    *)
+        section="$default_section"
+        ;;
+    esac
     manpages_file=debian/${pkg_name}.manpages
     build_mans "$list"
 done
 
+# simple option
 if [ "$found_list" != true ] && [ -f debian/genman-list ]
 then
     manpages_file=debian/manpages
