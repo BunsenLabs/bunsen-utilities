8799196ab58776255512447c8ec325d33095034c
johnraff <github@rafflesnagoya.com>
2020-05-04 18:53:17 +0900

    Ensure all executables have usable man pages
    - Tweak --help output as necessary.
    - Place parsing of --help option before any other unnecessary activity,
      to avoid errors in the restricted build environment.
    - Add some extra libraries to Build-Depends for the same reason.
    - Make bl-image-upload and bl-xinerama-prop man pages manually.
    - Add a new --makeone option to debian/genman.sh.

--- debian/genman.sh
+++ debian/genman.sh
@@ -4,7 +4,22 @@
 # to generate man files from executables' --help options,
 # using help2man.
 # Tested on Dash, should work with other POSIX shells.
-# Version 20190629
+# Version 20200503
+
+#    Copyright (C) 2018-2020  John Crawley <john@bunsenlabs.org>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Put this script in the package source debian/ directory
 # (or some other location, to taste),
@@ -50,7 +65,7 @@ default_section=1
 
 # BunsenLabs generic content
 # Other vendors, please edit to taste.
-includes="[authors]
+includes="[author]
 Written by the BunsenLabs team.
 
 [reporting bugs]
@@ -125,8 +140,7 @@ include_file=debian/genman.include
 
 # pass file with list of executables to make manpages for,
 # with file or shell glob on each line
-build_mans() {
-    local line file
+build_mans() (
     while read -r line
     do
         [ -f "$line" ] && {
@@ -139,14 +153,14 @@ build_mans() {
             mk_man "$file"
         done
     done < "$1"
-}
+)
 
 # pass "executable" file (it need not actually be executable in the source)
-mk_man() {
-    local exec="$1"
-    local cmd=${1##*/}
-    local manfile="${manpg_dir}/${cmd}.${section}"
-    local execflag=false
+mk_man() (
+    exec="$1"
+    cmd=${1##*/}
+    manfile="${manpg_dir}/${cmd}.${section}"
+    execflag=false
     mkdir -p "$manpg_dir"
     grep "${cmd}\(.[1-8]\)\? *\$" "${manpages_file}" >/dev/null 2>&1 && {
         echo "$0: ${cmd} already in ${manpages_file}, skipping"
@@ -160,22 +174,34 @@ mk_man() {
     help2man ./"$exec" --no-info --no-discard-stderr --version-string="$cmd $pkg_ver" --section="$section" --name="$desc" --include="$include_file" | sed "s|$HOME|~|g" > "$manfile"
     [ "$execflag" = false ] && chmod -x "$exec"
     echo "$manfile" >> "${manpages_file}"
-}
+)
 
 # pass "executable" file
 # output is piped to 'man -l -' for inspection
-test_man() {
-    local exec="$1"
-    local cmd=${1##*/}
-    local execflag=false
+# or by --create option to make single file in current directory
+test_man() (
+    createflag=false
+    [ "$1" = '--create' ] && {
+        createflag=true
+        shift
+    }
+    exec="$1"
+    cmd=${1##*/}
+    manfile=./"${cmd}.${section}"
+    execflag=false
     [ -x "$exec" ] && execflag=true
     [ "$execflag" = false ] && chmod +x "$exec"
     default_desc="a script provided by ${pkg_name}"
     desc="$( ./"$exec" --help 2>&1 | sed -rn "/^ *$cmd/ {s/^ *$cmd( -|:| is)? *//p;q}")"
     [ -z "$desc" ] && desc="$default_desc"
-    help2man ./"$exec" --no-info --no-discard-stderr --version-string="$cmd $pkg_ver" --section="$section" --name="$desc" --include="$include_file" | sed "s|$HOME|~|g" | man -l -
+    if [ "$createflag" = false ]
+    then
+        help2man ./"$exec" --no-info --no-discard-stderr --version-string="$cmd $pkg_ver" --section="$section" --name="$desc" --include="$include_file" | sed "s|$HOME|~|g" | man -l -
+    else
+        help2man ./"$exec" --no-info --no-discard-stderr --version-string="$cmd $pkg_ver" --section="$section" --name="$desc" --include="$include_file" | sed "s|$HOME|~|g" > "$manfile"
+    fi
     [ "$execflag" = false ] && chmod -x "$exec"
-}
+)
 
 ### script starts here ###
 
@@ -200,6 +226,17 @@ case $1 in
 $includes
 EOF
     test_man "$2"
+    rm -f "$include_file"
+    exit
+    ;;
+--makeone)
+    [ -n "$2" ] || { echo "Please pass a file to make a manpage for." >&2; exit 1;}
+    section="$default_section"
+    include_file="$(mktemp)"
+    cat <<EOF > "$include_file"
+$includes
+EOF
+    test_man --create "$2"
     rm -f "$include_file"
     exit
     ;;
